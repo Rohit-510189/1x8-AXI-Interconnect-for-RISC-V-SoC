53a176b44b7bd953db40f04dcd3b761a487631c7
Rohit <rohitreddy0005@gmail.com>
2026-08-29 17:40:33 +0530

    Add RISC-V SoC AXI interconnect project

---
+++ run/filelist.f
@@ -0,0 +1,5 @@
+/home/student/Documents/HC_Proj_095/rtl/interconnect/priority_encoder.v
+/home/student/Documents/HC_Proj_095/rtl/interconnect/arbiter.v
+/home/student/Documents/HC_Proj_095/rtl/interconnect/axi_interconnect.v
+/home/student/Documents/HC_Proj_095/rtl/interconnect/axi_interconnect_wrap_1x8.v
+/home/student/Documents/HC_Proj_095/rtl/interconnect/tb_axi_interconnect_wrap_1x8.v
